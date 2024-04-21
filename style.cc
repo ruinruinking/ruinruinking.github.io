88086f0ce9b7132da3c3c468768add9874ac04b2
ruinruinking <141360757+ruinruinking@users.noreply.github.com>
2024-04-20 20:19:40 -0500

    Create style.cc

---
+++ style.cc
@@ -0,0 +1,15 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 40px;
+    background-color: #f4f4f9;
+    text-align: center;
+}
+
+h1 {
+    color: #333;
+}
+
+p {
+    font-size: 20px;
+    color: #666;
+}
